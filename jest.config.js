5c7a6cfba67283a9f8ac044807f4d058320b0971
jw20191n <clairejw1015@gmail.com>
2020-11-24 19:38:02 -0500

    store app added

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,12 @@ module.exports = {
     '<rootDir>/apps/review',
     '<rootDir>/apps/api',
     '<rootDir>/libs/api-interfaces',
+    '<rootDir>/libs/review/ui-tile',
+    '<rootDir>/libs/review/util-formatters',
+    '<rootDir>/libs/review/data-access-games',
+    '<rootDir>/libs/review/feature-lib',
+    '<rootDir>/libs/review/feature-details',
+    '<rootDir>/libs/review/feature-list',
+    '<rootDir>/apps/store',
   ],
 };
